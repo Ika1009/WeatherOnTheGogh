0ac7a021c59b7529e6bdbf5254a334e1e472c1fc
milosmitrovic20 <milosmitrovic005@gmail.com>
2025-01-18 13:59:31 +0100

    update title and stylesheet, remove unused styles, and enhance layout for weather UI

---
+++ data.js
@@ -0,0 +1,172 @@
+const weatherCategories = {
+    "200_Thunderstorm_thunderstorm with light rain": [
+        "200 - Day - The Plain at Auvers (1890).mp4",
+        "200 - Day - Wheatfield under Thunderclouds.mp4",
+        "200 - Sunset - View of the Sea at Scheveningen.mp4"
+    ],
+    "202_Thunderstorm_thunderstorm with heavy rain": [
+        "202 - Day - Rain (1889).mp4",
+        "202 - Sunset - View of the Sea at Scheveningen.mp4"
+    ],
+    "230_Thunderstorm_thunderstorm with light drizzle": [
+        "230 - Day - Wheatfield with Crows.mp4",
+        "230 - Sunset - View of the Sea at Scheveningen.mp4"
+    ],
+    "300_Drizzle_light intensity drizzle": [
+        "300 - Day - The Plain at Auvers (1890).mp4"
+    ],
+    "301_Drizzle_drizzle": [
+        "301 - Day - The Outskirts of Paris (1886).mp4"
+    ],
+    "500_Rain_light rain": [
+        "500 - Day - Rain (1889).mp4",
+        "500 - Sunset - View of the Sea at Scheveningen.mp4"
+    ],
+    "501_Rain_moderate rain": [
+        "501 - Day - Landscape at Auvers in the Rain (1890).mp4",
+        "501 - Day - Rain (1889).mp4",
+        "501 - Day - Sower in the Rain.mp4",
+        "501 - Day - Three Men Shouldering Spades on a Road in the Rain.mp4",
+        "501 - Sunset - Landscape at Auvers in the Rain (1890).mp4",
+        "501 - Sunset - Rain (1889).mp4"
+    ],
+    "502_Rain_heavy intensity rain": [
+        "502 - Day - Bridge in the Rain (after Hiroshige).mp4",
+        "502 - Day - Rain (1889).mp4",
+        "502 - Sunset - Landscape at Auvers in the Rain (1890).mp4"
+    ],
+    "503_Rain_very heavy rain": [
+        "503 - Day - View of Paris from Vincent's Room in the Rue Lepic.mp4",
+        "503 - Day - Rain (1889).mp4",
+        "503 - Sunset - Landscape at Auvers in the Rain (1890).mp4"
+    ],
+    "521_Rain_shower rain": [
+        "521 - Day - Couple with Child, Walking in the Rain.mp4",
+        "521 - Day - Pollard Willow.mp4"
+    ],
+    "600_Snow_light snow": [
+        "600 - Day - Snow-Covered Field with a Harrow (after Millet).mp4"
+    ],
+    "601_Snow_snow": [
+        "601 - Day - The Vicarage Garden in the Snow (1885) Sketch.mp4",
+        "601 - Day - Winter (The Vicarage Garden under Snow).mp4"
+    ],
+    "602_Snow_heavy snow": [
+        "602 - Day - Landscape with Snow (1888).mp4"
+    ],
+    "622_Snow_heavy shower snow": [
+        "622 - Day - The Old Cemetery Tower at Nuenen in the Snow.mp4"
+    ],
+    "701_Mist_mist": [
+        "701 - Day - Avenue of Poplars.mp4",
+        "701 - Day - Congregation Leaving the Reformed Church in Nuenen.mp4",
+        "701 - Day - Factories at Asnières (1887).mp4",
+        "701 - Day - Impasse des Deux Frères.mp4",
+        "701 - Day - The De Ruijterkade in Amsterdam.mp4",
+        "701 - Day - The Old Church Tower at Nuenen (The Peasants' Churchyard).mp4",
+        "701 - Sunset - The Old Church Tower at Nuenen (The Peasants' Churchyard).mp4",
+        "701 - Sunset - The Vicarage at Nuenen.mp4"
+    ],
+    "721_Haze_haze": [
+        "712 - Day - Impasse des Deux Frères.mp4",
+        "712 - Day - Montmartre Behind the Moulin de la Galette.mp4",
+        "712 - Day - The Langlois Bridge at Arles.mp4"
+    ],
+    "741_Fog_fog": [
+        "741 - Day - Autumn Landscape with Four Trees (1885).mp4",
+        "741 - Day - Impasse des Deux Frères.mp4"
+    ],
+    "771_Squall_squalls": [
+        "771 - Day - Seascape near Les Saintes-Maries-de-la-Mer.mp4",
+        "771 - Day - View of the Sea at Scheveningen.mp4",
+        "771 - Day - Wheatfield under Thunderclouds.mp4"
+    ],
+    "800_Clear_clear sky": [
+        "800 - Day - Allotment with Sunflower.mp4",
+        "800 - Day - Avenue of Poplars.mp4",
+        "800 - Day - Bank of the Seine.mp4",
+        "800 - Day - By the Seine.mp4",
+        "800 - Day - The Harvest.mp4",
+        "800 - Day - The White Orchard.mp4",
+        "800 - Day - View of Auvers-sur-Oise.mp4",
+        "800 - Day - Wheatfield with a Reaper.mp4",
+        "800 - Day - Wheatfield.mp4",
+        "800 - Night - Cafe Terrace at Night.mp4",
+        "800 - Night - Starry Night Over the Rhône.mp4",
+        "800 - Night - The Yellow House (The Street).mp4"
+    ],
+    "801_Clouds_few clouds 11 - 25": [
+        "801 - Day - Allotment with Sunflower.mp4",
+        "801 - Day - Field with Irises near Arles.mp4",
+        "801 - Day - Field with Poppies.mp4",
+        "801 - Day - Montmartre Windmills and Allotments.mp4",
+        "801 - Day - Orchards in Blossom, View of Arles.mp4",
+        "801 - Day - Peasant Woman Binding Sheaves (after Millet).mp4",
+        "801 - Day - The Bridge at Courbevoie.mp4",
+        "801 - Day - The Pink Orchard.mp4",
+        "801 - Day - The Pink Peach Tree.mp4",
+        "801 - Day - The Raising of Lazarus (after Rembrandt).mp4",
+        "801 - Day - The Reaper (after Millet).mp4",
+        "801 - Day - The Sheaf-Binder (after Millet).mp4",
+        "801 - Day - The Sower.mp4",
+        "801 - Day - The White Orchard.mp4",
+        "801 - Day - Wheatfield.mp4",
+        "801 - Night - The Yellow House (The Street).mp4",
+        "801 - Sunset - Orchard in Blossom.mp4",
+        "801 - Sunset - The Bridge at Courbevoie.mp4",
+        "801 - Sunset - The Reaper (after Millet).mp4",
+        "801 - Sunset - The Sower.mp4"
+    ],
+    "802_Clouds_scattered clouds 25-50": [
+        "802 - Day - Allotment with Sunflower.mp4",
+        "802 - Day - Bank of the Seine.mp4",
+        "802 - Day - Boulevard de Clichy.mp4",
+        "802 - Day - Farmhouse in a Wheatfield.mp4",
+        "802 - Day - Field with Irises near Arles.mp4",
+        "802 - Day - Field with Poppies.mp4",
+        "802 - Day - Fishing Boats on the Beach at Les Saintes-Maries-de-la-Mer.mp4",
+        "802 - Day - Garden of the Asylum.mp4",
+        "802 - Day - Garden with Courting Couples Square Saint-Pierre.mp4",
+        "802 - Day - Gate in the Paris Ramparts.mp4",
+        "802 - Day - Impasse des Deux Frères.mp4",
+        "802 - Day - Montmartre Behind the Moulin de la Galette.mp4",
+        "802 - Day - Old Vineyard with Peasant Woman.mp4",
+        "802 - Day - Ploughed Fields (The Furrows).mp4",
+        "802 - Day - View from Theo's Apartment.mp4",
+        "802 - Day - View of the Alpilles.mp4",
+        "802 - Day - View of the Sea at Scheveningen.mp4",
+        "802 - Sunset - Landscape at Twilight.mp4",
+        "802 - Sunset - Olive Grove.mp4",
+        "802 - Sunset - Path in Montmartre.mp4",
+        "802 - Sunset - Square Saint-Pierre at Sunset.mp4",
+        "802 - Sunset - Sunset in Montmartre.mp4"
+    ],
+    "803_Clouds_broken clouds 51-84": [
+        "803 - Day - Boulevard de Clichy.mp4",
+        "803 - Day - Farmhouse in a Wheatfield.mp4",
+        "803 - Day - Garden with Courting Couples Square Saint-Pierre.mp4",
+        "803 - Day - The Hill of Montmartre with Stone Quarry.mp4",
+        "803 - Night - Olive Grove.mp4",
+        "803 - Night - The Starry Night.mp4",
+        "803 - Night - Wheatfield with Crows.mp4",
+        "803 - Sunset - Olive Grove.mp4",
+        "803 - Sunset - View from Vincent's Studio.mp4"
+    ],
+    "804_Clouds_overcast clouds 85-100": [
+        "804 - Day - Farm with Stacks of Peat.mp4",
+        "804 - Day - Houses Seen from the Back.mp4",
+        "804 - Day - The Old Church Tower at Nuenen (The Peasants' Churchyard).mp4",
+        "804 - Day - View of Paris.mp4",
+        "804 - Day - Women on the Peat Moor.mp4",
+        "804 - Night - The Starry Night.mp4",
+        "804 - Night - Wheatfield with Crows.mp4",
+        "804 - Sunset - Congregation Leaving the Reformed Church in Nuenen.mp4",
+        "804 - Sunset - Cottages.mp4",
+        "804 - Sunset - Farm with Stacks of Peat.mp4",
+        "804 - Sunset - Reminiscence of Brabant.mp4",
+        "804 - Sunset - The Cottage.mp4",
+        "804 - Sunset - Women on the Peat Moor.mp4"
+    ]
+};
+
+export default weatherCategories;
